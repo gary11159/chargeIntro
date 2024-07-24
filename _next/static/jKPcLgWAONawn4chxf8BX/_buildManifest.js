a51a4e5f2717c76b3aa952fd615ce98323ffe8fa
gary11159 <gary11159@gmail.com>
2024-07-25 02:03:03 +0800

    Updates

---
+++ _next/static/jKPcLgWAONawn4chxf8BX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/747-3ca5522fef22d4d0.js","static/chunks/pages/index-86a981ef88f834ef.js"],"/_error":["static/chunks/pages/_error-3c9a37b9888d2d4e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/xwVUXTks-PdjRkcyECm4O/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/540-61f17c29b0036ca6.js","static/chunks/pages/index-35e3ba01980f3ad5.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
